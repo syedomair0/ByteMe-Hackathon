b5b2497775feba222cd7cfe46c5835b8765523ac
mkalal6 <137332780+mkalal6@users.noreply.github.com>
2024-03-03 09:51:06 -0600

    welcome page updates

--- byte-me/src/components/Home.js
+++ byte-me/src/components/Home.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import background from '../assets/waves.svg'
 import people from '../assets/people.png';
 import logo from '../assets/logo.jpeg';
+import Heart from "react-animated-heart"; 
 
 const Home = (props) => {
   const { loggedIn, email } = props
@@ -11,20 +12,24 @@ const Home = (props) => {
   const onButtonClick = () => {
     // You'll update this function later
   }
+  const [isClick, setClick] = useState(false);
 
   return (
     <div className={'bg'} style={{backgroundImage: `url(${background})`}}>
       <div className="mainContainer">
-        <div className={'welcomeTitleContainer'}>
-          <div>Welcome!</div>
+        <div className="welcomeTitleContainer">
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div>Welcome!</div>
+            <img src={logo} alt="Logo" style={{ width: '80px', height: 'auto', marginLeft: '20px' }} /> 
+          </div>
         </div>
         <div className={'elevatorPitchContainer'}>
           <div>Where Love Takes the Lead! Your go-to app for spontaneous, fun-filled date plans tailored just for you. Let's make every date an adventure!</div>
         </div>
-        <div className="logoContainer">
-          <img src={logo} alt="Logo" style={{ width: '100px', height: 'auto', marginLeft: '20px' }} /> 
-        </div>
         <img src={people} alt="People" style={{ width: '350px', height: 'auto', marginTop: '20px' }} />
+      </div>
+      <div className="heart" style={{ textAlign: 'center', marginTop: '20px' }}>
+        <Heart isClick={isClick} onClick={() => setClick(!isClick)} style={{ height: '60px', width: '400px', fontSize: 'large'}} />
       </div>
     </div>
   );
